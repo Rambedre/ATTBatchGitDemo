7b92b23377559f378d565e0441928074a59ab8e1
Rambedre <rambedre621@gmail.com>
2024-09-01 22:17:59 +0530

    First commit 22.17

---
+++ src/ABC/DemoClass1.java
@@ -0,0 +1,12 @@
+package ABC;
+
+public class DemoClass1 {
+	
+	public static void main(String[] args) {
+		System.out.println("This is Demo Class");
+		System.out.println("User enter userID as 12314124124");
+		System.out.println("UserEnter password as 12124124");
+		System.out.println("User click on Login Btn");
+		System.out.println("user on HomePage");
+	}
+}
