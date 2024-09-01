8e98a36e315542420c11042c89c74930919d1bc9
Rambedre <rambedre621@gmail.com>
2024-09-01 23:32:43 +0530

    Commited by owner 23.32

--- src/ABC/DemoClass1.java
+++ src/ABC/DemoClass1.java
@@ -4,7 +4,7 @@ public class DemoClass1 {
 	
 	public static void main(String[] args) {
 		System.out.println("This is Demo Class");
-		System.out.println("User enter userID as 12314124124");
+		System.out.println("User enter userID as ABC1234");
 		System.out.println("UserEnter password as 12124124");
 		System.out.println("User click on Login Btn");
 		System.out.println("user on HomePage");
