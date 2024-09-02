0d21830f3d3776bc7b186afecea3a7737643b8ee
Rambedre <rambedre621@gmail.com>
2024-09-02 22:58:59 +0530

    Commit 22.58

--- src/ABC/DemoClass1.java
+++ src/ABC/DemoClass1.java
@@ -9,14 +9,7 @@ public class DemoClass1 {
 		System.out.println("User click on Login Btn");
 		System.out.println("user on HomePage");
 		System.out.println("Changes Done by User 3");
-<<<<<<< HEAD
-		System.out.println("Changes Done by User 2");
-=======
 		System.out.println("One More Change by User 3");
-<<<<<<< HEAD
->>>>>>> 34be6f6685fcc997365ef7e1eafcfb72d2a27d4f
-=======
 		System.out.println("Changes Done By Master USer");
->>>>>>> 34fe8bae6b382fd6825a0977dec8bd3dae104702
 	}
 }
